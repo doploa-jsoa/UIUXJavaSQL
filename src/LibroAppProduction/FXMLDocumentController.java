7109e7ea719a315b4555bdfd6d709df4451a9252
doploa-jsoa <36301837+doploa-jsoa@users.noreply.github.com>
2018-02-13 14:40:44 +0000

    Add files via upload
    Edits to the code to allow for multiple column tableView

--- src/LibroAppProduction/FXMLDocumentController.java
+++ src/LibroAppProduction/FXMLDocumentController.java
@@ -27,14 +27,19 @@ import javafx.scene.control.cell.PropertyValueFactory;
 public class FXMLDocumentController implements Initializable {
     
      public class Record{
-      private SimpleStringProperty fieldMonth;
-      private SimpleDoubleProperty fieldValue;
+      private final SimpleStringProperty fieldMonth;
+      private final SimpleDoubleProperty fieldValue;
+      private final SimpleDoubleProperty fieldValue2;
+      private final SimpleDoubleProperty fieldValue3;
      
-      Record(String fMonth, double fValue){
+      Record(String fMonth, double fValue, double fValue2, double fValue3){
           this.fieldMonth = new SimpleStringProperty(fMonth);
+          //this.fieldNewDay = new SimpleDoubleProperty(fValue);
           this.fieldValue = new SimpleDoubleProperty(fValue);
+          this.fieldValue2 = new SimpleDoubleProperty(fValue2);
+          this.fieldValue3 = new SimpleDoubleProperty(fValue3);
       }
-     
+      
       public String getFieldMonth() {
           return fieldMonth.get();
       }
@@ -42,23 +47,31 @@ public class FXMLDocumentController implements Initializable {
       public double getFieldValue() {
           return fieldValue.get();
       }
+      
+      public double getFieldValue2() {
+          return fieldValue2.get();
+      }
+      
+      public double getFieldValue3() {
+          return fieldValue3.get();
+      }
      
   }
      
      private ObservableList<Record> dataList =
           FXCollections.observableArrayList(          
-              new Record("January", 100),
-              new Record("February", 200),
-              new Record("March", 50),
-              new Record("April", 75),
-              new Record("May", 110),
-              new Record("June", 300),
-              new Record("July", 111),
-              new Record("August", 30),
-              new Record("September", 75),
-              new Record("October", 55),
-              new Record("November", 225),
-              new Record("December", 99));
+              new Record("January", 100, 50, 70),
+              new Record("February", 200 , 50, 70),
+              new Record("March", 50, 50, 70),
+              new Record("April", 75, 50, 70),
+              new Record("May", 110, 50, 70),
+              new Record("June", 300, 50, 70),
+              new Record("July", 111, 30, 70),
+              new Record("August", 30, 250, 70),
+              new Record("September", 75, 90, 70),
+              new Record("October", 55, 70, 70),
+              new Record("November", 225, 60, 70),
+              new Record("December", 99, 50, 70));
      
      
     @FXML
@@ -75,23 +88,35 @@ public class FXMLDocumentController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-       //primaryStage.setTitle("");
+   
      
       //Group root = new Group();
- 
+      
       TableColumn columnMonth = new TableColumn("Month");
-      columnMonth.setCellValueFactory(
-              new PropertyValueFactory<Record,String>("fieldMonth"));
+        columnMonth.setCellValueFactory(
+                new PropertyValueFactory<Record,String>("fieldMonth"));
+        columnMonth.setMinWidth(60);
+        
+        TableColumn columnValue1 = new TableColumn("Value 1");
+        columnValue1.setCellValueFactory(
+                new PropertyValueFactory<Record,Double>("fieldValue"));
+        columnValue1.setMinWidth(60);
+        
+        TableColumn columnValue2 = new TableColumn("Value 2");
+        columnValue2.setCellValueFactory(
+                new PropertyValueFactory<Record,Double>("fieldValue2"));
+        columnValue2.setMinWidth(60);
+        
+        TableColumn columnValue3 = new TableColumn("Value 3");
+        columnValue3.setCellValueFactory(
+                new PropertyValueFactory<Record,Double>("fieldValue3"));
+        columnValue3.setMinWidth(60);
  
-      TableColumn columnValue = new TableColumn("Value");
-      columnValue.setCellValueFactory(
-              new PropertyValueFactory<Record,Double>("fieldValue"));
-      
-      
+
      
       tableView.setItems(dataList);
       //tableView.getColumns().addAll(columnMonth, columnValue);
-      tableView.getColumns().setAll(columnMonth, columnValue);
+      tableView.getColumns().addAll(columnMonth, columnValue1, columnValue2, columnValue3);
         // TODO
     }    
     
