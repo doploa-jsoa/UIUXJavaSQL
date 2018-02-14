3006c5da761b3d4c1c7d904663ad6b43f8edfdc6
doploa-jsoa <36301837+doploa-jsoa@users.noreply.github.com>
2018-02-14 17:41:56 +0000

    Add files via upload
    Multiple column editing with data formatting

--- src/LibroAppProduction/FXMLDocumentController.java
+++ src/LibroAppProduction/FXMLDocumentController.java
@@ -8,6 +8,7 @@ package LibroAppProduction;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -28,50 +29,63 @@ public class FXMLDocumentController implements Initializable {
     
      public class Record{
       private final SimpleStringProperty fieldMonth;
-      private final SimpleDoubleProperty fieldValue;
-      private final SimpleDoubleProperty fieldValue2;
-      private final SimpleDoubleProperty fieldValue3;
+      private final SimpleStringProperty fieldValue;
+      private final SimpleIntegerProperty fieldValue2;
+      private final SimpleIntegerProperty fieldValue3;
+      private final SimpleIntegerProperty fieldValue4;
+      private final SimpleStringProperty fieldValue5;
      
-      Record(String fMonth, double fValue, double fValue2, double fValue3){
+      Record(String fMonth, String fValue, int fValue2, int fValue3, int fValue4, String fValue5){
           this.fieldMonth = new SimpleStringProperty(fMonth);
           //this.fieldNewDay = new SimpleDoubleProperty(fValue);
-          this.fieldValue = new SimpleDoubleProperty(fValue);
-          this.fieldValue2 = new SimpleDoubleProperty(fValue2);
-          this.fieldValue3 = new SimpleDoubleProperty(fValue3);
+          this.fieldValue = new SimpleStringProperty(fValue);
+          this.fieldValue2 = new SimpleIntegerProperty(fValue2);
+          this.fieldValue3 = new SimpleIntegerProperty(fValue3);
+          this.fieldValue4 = new SimpleIntegerProperty(fValue4);
+          this.fieldValue5 = new SimpleStringProperty(fValue5);
       }
       
       public String getFieldMonth() {
           return fieldMonth.get();
       }
      
-      public double getFieldValue() {
+      public String getFieldValue() {
           return fieldValue.get();
       }
       
-      public double getFieldValue2() {
+      public int getFieldValue2() {
           return fieldValue2.get();
       }
       
-      public double getFieldValue3() {
+      public int getFieldValue3() {
           return fieldValue3.get();
       }
+      
+      public int  getFieldValue4() {
+          return fieldValue4.get();
+      }
+      
+      public String getFieldValue5() {
+          return fieldValue5.get();
+      }
+     
      
   }
      
      private ObservableList<Record> dataList =
           FXCollections.observableArrayList(          
-              new Record("January", 100, 50, 70),
-              new Record("February", 200 , 50, 70),
-              new Record("March", 50, 50, 70),
-              new Record("April", 75, 50, 70),
-              new Record("May", 110, 50, 70),
-              new Record("June", 300, 50, 70),
-              new Record("July", 111, 30, 70),
-              new Record("August", 30, 250, 70),
-              new Record("September", 75, 90, 70),
-              new Record("October", 55, 70, 70),
-              new Record("November", 225, 60, 70),
-              new Record("December", 99, 50, 70));
+              new Record("A Wrinkle In Time", "Author1", 50, 100, 50, "Publisher1"),
+              new Record("February", "Author1", 50,200 , 50, "Publisher1"),
+              new Record("March", "Author1", 50,50, 50, "Publisher1"),
+              new Record("April","Author1", 50, 75, 50, "Publisher1"),
+              new Record("May", "Author1", 50,110, 50, "Publisher1"),
+              new Record("June", "Author1", 50,300, 50, "Publisher1"),
+              new Record("July", "Author1", 50,111, 30, "Publisher1"),
+              new Record("August", "Author1", 50, 30, 250, "Publisher1"),
+              new Record("September", "Author1", 50, 75, 90, "Publisher1"),
+              new Record("October", "Author1", 50,55, 70, "Publisher1"),
+              new Record("November", "Author1", 50,225, 60, "Publisher1"),
+              new Record("December", "Author1", 50,99, 50, "Publisher1"));
      
      
     @FXML
@@ -92,31 +106,40 @@ public class FXMLDocumentController implements Initializable {
      
       //Group root = new Group();
       
-      TableColumn columnMonth = new TableColumn("Month");
+      TableColumn columnMonth = new TableColumn("Title");
         columnMonth.setCellValueFactory(
                 new PropertyValueFactory<Record,String>("fieldMonth"));
         columnMonth.setMinWidth(60);
         
-        TableColumn columnValue1 = new TableColumn("Value 1");
+        TableColumn columnValue1 = new TableColumn("Author");
         columnValue1.setCellValueFactory(
-                new PropertyValueFactory<Record,Double>("fieldValue"));
+                new PropertyValueFactory<Record,Integer>("fieldValue"));
         columnValue1.setMinWidth(60);
         
-        TableColumn columnValue2 = new TableColumn("Value 2");
+        TableColumn columnValue2 = new TableColumn("Pages");
         columnValue2.setCellValueFactory(
-                new PropertyValueFactory<Record,Double>("fieldValue2"));
+                new PropertyValueFactory<Record,Integer>("fieldValue2"));
         columnValue2.setMinWidth(60);
         
-        TableColumn columnValue3 = new TableColumn("Value 3");
+        TableColumn columnValue3 = new TableColumn("ISBN");
         columnValue3.setCellValueFactory(
-                new PropertyValueFactory<Record,Double>("fieldValue3"));
+                new PropertyValueFactory<Record,String>("fieldValue3"));
         columnValue3.setMinWidth(60);
  
+        TableColumn<Record,Integer> columnValue4 = new TableColumn("Year");
+        columnValue4.setCellValueFactory(
+                new PropertyValueFactory<Record,Integer>("fieldValue4"));
+        columnValue4.setMinWidth(60);
+        
+        TableColumn columnValue5 = new TableColumn("Publisher");
+        columnValue5.setCellValueFactory(
+                new PropertyValueFactory<Record,Double>("fieldValue5"));
+        columnValue5.setMinWidth(60);
 
      
       tableView.setItems(dataList);
       //tableView.getColumns().addAll(columnMonth, columnValue);
-      tableView.getColumns().addAll(columnMonth, columnValue1, columnValue2, columnValue3);
+      tableView.getColumns().setAll(columnMonth, columnValue1, columnValue2, columnValue3, columnValue4, columnValue5);
         // TODO
     }    
     
